43ee28ecb223769ba75068bac16669ff07a0dcdc
zjy201220166 <1455596479@qq.com>
2022-11-25 16:59:50 +0800

    2nd output

--- output.cpp
+++ output.cpp
@@ -2,5 +2,27 @@
 
 Output::Output()
 {
+ans.clear();
+}
+void Output::addans(QString s)
+{
+    ans.push_back(s);
+}
+void Output::writeinfile()
+{
+    QFile file("output_equal.csv");
+    if(!file.open(QIODevice::WriteOnly))
+    {
+        qDebug()<<"write open failed!";
+        return;
+    }
+    QTextStream out(&file);
+    for(int i=0;i<ans.size();i++)
+    {
+        out<<ans[i]<<endl;
+    }
+    file.close();
+    return;
+
 
 }
--- output.h
+++ output.h
@@ -1,11 +1,18 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
-
-
+#include<QString>
+#include<QFile>
+#include<QTextStream>
+#include<QVector>
+#include<QDebug>
 class Output
 {
+private:
+    QVector<QString>ans;
 public:
     Output();
+    void addans(QString s);
+    void writeinfile();
 };
 
 #endif // OUTPUT_H
